aa7a263e1a7a9acf8fe4aedc6b01cb5169a9bf92
mr_Larson <gauthierdewit@gmail.com>
2021-06-16 14:45:40 +0200

    gate ok

--- laravel-preparation-labs/app/Providers/AuthServiceProvider.php
+++ laravel-preparation-labs/app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
@@ -25,6 +26,81 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        //Gate User
+        Gate::define('user-create', function () {
+            return in_array(Auth::user()->role_id, [1]);
+        });
+
+        Gate::define('user-edit', function () {
+            return in_array(Auth::user()->role_id, [1]);
+        });
+
+        //Gate Hero
+        Gate::define('hero-create', function () {
+            return in_array(Auth::user()->role_id, [1]);
+        });
+
+        Gate::define('hero-edit', function () {
+            return in_array(Auth::user()->role_id, [1]);
+        });
+
+        //Gate About
+        Gate::define('about-create', function () {
+            return in_array(Auth::user()->role_id, [1]);
+        });
+
+        Gate::define('about-edit', function () {
+            return in_array(Auth::user()->role_id, [1]);
+        });
+
+        //Gate Feature
+        Gate::define('feature-create', function () {
+            return in_array(Auth::user()->role_id, [1]);
+        });
+
+        Gate::define('feature-edit', function () {
+            return in_array(Auth::user()->role_id, [1]);
+        });
+
+        //Gate portfolio
+        Gate::define('portfolio-create', function () {
+            return in_array(Auth::user()->role_id, [1]);
+        });
+
+        Gate::define('portfolio-edit', function () {
+            return in_array(Auth::user()->role_id, [1]);
+        });
+
+        //Gate Service
+        Gate::define('service-create', function () {
+            return in_array(Auth::user()->role_id, [1]);
+        });
+
+        Gate::define('service-edit', function () {
+            return in_array(Auth::user()->role_id, [1]);
+        });
+
+        //Gate Testimonial
+        Gate::define('testimonial-create', function () {
+            return in_array(Auth::user()->role_id, [1]);
+        });
+
+        Gate::define('testimonial-edit', function () {
+            return in_array(Auth::user()->role_id, [1]);
+        });
+
+        //Gate link
+        Gate::define('link-create', function () {
+            return in_array(Auth::user()->role_id, [1]);
+        });
+
+        Gate::define('link-edit', function () {
+            return in_array(Auth::user()->role_id, [1]);
+        });
+
+        //Gate Adresse
+        Gate::define('adresse', function () {
+            return in_array(Auth::user()->role_id, [1]);
+        });
     }
 }
